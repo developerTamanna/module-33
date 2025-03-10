20457e7e11b36bbc8214d0b61f7ff2ea9c0f9005
developerTamanna <astamanna57@gmail.com>
2025-03-10 11:49:09 +0600

    stringify

---
+++ script/stringify.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'lima',
+    age: 32,
+    city: 'sylhet',
+    status: true
+};
+
+const userString = JSON.stringify(user);
+// console.log(typeof user)
+// console.log(typeof userString)
+console.log(userString)
